ea4096a69c75b73110417b592fcbdc4c179f43c5
Electrolyte & Flare <115198837+elyctstudios@users.noreply.github.com>
2024-06-15 22:23:52 -0600

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,3 +1,24 @@
-local asd = {
-  "SevereOath"
+local WHITELIST = {}
+
+local Players = {
+	"f4aculty"
 }
+
+local Player = game.Players.LocalPlayer.Name
+
+function WHITELIST:Check()
+	local Triggered = false
+	for i, v in pairs(Players) do
+		if v == Player then
+			Triggered = true
+		end
+	end
+
+	if Triggered == true then
+		return true
+	else
+		return false
+	end
+end
+
+return WHITELIST
